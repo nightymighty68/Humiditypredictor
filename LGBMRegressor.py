5e3acca0421839ffcc1f4fdb1f1c28045b389776
Mohammed Hujairi <75939400+nightymighty68@users.noreply.github.com>
2024-11-25 21:24:41 +0300

    Update LGBMRegressor.py

--- LGBMRegressor.py
+++ LGBMRegressor.py
@@ -6,7 +6,7 @@ from sklearn.impute import SimpleImputer
 import xgboost as xgb
 
 # Load the dataset
-df = pd.read_csv('tech-olympiad-2024-bahrain-zain-challenge/TestData.csv', delimiter='|')
+df = pd.read_csv('tech-olympiad-2024-bahrain-zain-challenge/TrainData.csv', delimiter='|')
 
 features = ['CO(GT)', 'PT08.S1(CO)', 'PT08.S2(NMHC)', 'PT08.S2_NMHC_to_PT08.S1_CO_Ratio',
             'NMHC(GT)', 'NOx(GT)', 'PT08.S3(NOx)', 'NO2(GT)', 'PT08.S4(NO2)', 'PT08.S5(O3)']
